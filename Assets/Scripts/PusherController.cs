c700771728d7a8ef719c5e3bcf70615d43e9dd65
張彪 <z.angus0121@gmail.com>
2016-10-24 01:32:39 +0900

    キャンディと土台を作成

---
+++ Assets/Scripts/PusherController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PusherController : MonoBehaviour {
+
+	Vector3 startPosition;
+
+	// 移動量のパラメータ
+	public float amplitude;
+	// 移動速度のパラメータ
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+		startPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// 変位を計算
+		float z = amplitude * Mathf.Sin (Time.time * speed);
+
+		// zを変化させたポジションに再設定
+		transform.localPosition = startPosition + new Vector3(0, 0, z);
+	}
+}
